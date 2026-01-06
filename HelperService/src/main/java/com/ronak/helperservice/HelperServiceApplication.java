1a90ac1b5df71c008955007351d84fccb4d5246b
ronak <chaudharyronak291@gmail.com>
2026-01-06 10:39:40 +0530

    add initial project structure with Maven configuration and Spring Boot setup

---
+++ HelperService/src/main/java/com/ronak/helperservice/HelperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ronak.helperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperServiceApplication.class, args);
+    }
+
+}
