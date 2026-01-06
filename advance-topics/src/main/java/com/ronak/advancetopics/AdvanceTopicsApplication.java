f9e5f77fb1421b801afb58a51d1276e60a15ac65
ronak <chaudharyronak291@gmail.com>
2026-01-06 10:09:42 +0530

    initialize Spring Boot application with basic configuration and project structure

---
+++ advance-topics/src/main/java/com/ronak/advancetopics/AdvanceTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.ronak.advancetopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceTopicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceTopicsApplication.class, args);
+    }
+
+}
---
+++ advance-topics/src/test/java/com/ronak/advancetopics/AdvanceTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronak.advancetopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceTopicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
